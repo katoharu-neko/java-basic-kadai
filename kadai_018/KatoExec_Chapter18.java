06b013e876dee09f06db943cfb0698ef3d970f8c
katoharu-neko <haruki.katoh.0@gmail.com>
2025-07-11 19:12:06 +0900

    kadai018

---
+++ kadai_018/KatoExec_Chapter18.java
@@ -0,0 +1,25 @@
+package kadai_018;
+
+public class KatoExec_Chapter18 {
+
+	public static void main(String[] args) {
+		KatoTaro_Chapter18 katoTaro = new KatoTaro_Chapter18();
+		katoTaro.setGivenName();
+		katoTaro.execIntroduce();
+		
+		System.out.println();
+		
+		KatoIchiro_Chapter18 katoIchiro = new KatoIchiro_Chapter18();
+		katoIchiro.setGivenName();
+		katoIchiro.execIntroduce();
+		
+		System.out.println();
+		
+		KatoHanako_Chapter18 katoHanako = new KatoHanako_Chapter18();
+		katoHanako.setGivenName();
+		katoHanako.execIntroduce();
+		
+
+	}
+
+}
